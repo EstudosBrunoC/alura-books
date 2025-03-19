420eb8d25cb64b482f1836659050e3f88dbbf6d5
brucantacini <brunocantacini.ti@gmail.com>
2025-03-19 11:10:10 -0300

    Utilizando styled-components para otimizar códigos.

--- src/components/OpcoesHeader/index.js
+++ src/components/OpcoesHeader/index.js
@@ -1,13 +1,28 @@
-import './style.css';
+import styled from "styled-components";
+const Opcoes = styled.ul`
+    display: flex;
+`;
+const Opcao = styled.li`
+    font-size: 16px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    height: 100%;
+    padding: 0.5px;
+    cursor: pointer;
+    min-width: 120px;
+`;
+
 const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE'];
 
 function OpcoesHeader () {
     return (
-        <ul className='opcoes'>
+        <Opcoes>
             {textoOpcoes.map( (opcao) => (
-                <li className='opcao'><p>{opcao}</p></li>
+                <Opcao><p>{opcao}</p></Opcao>
             ) ) }
-        </ul>
+        </Opcoes>
     )
 }
 
